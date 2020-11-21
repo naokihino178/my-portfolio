1286e54ae51d01ad43e19a7bc0ae5ef1698e1a41
naokihino178 <naoki.hino178@gmail.com>
2020-11-21 21:30:25 +0900

    next.jsチュートリアル（Two Forms of Pre-renderingまで）

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+/* global.cssは_app.jsにしかインポートできない！！ */
+
+// 特に宣言とかをしなくても、全てのコンポーネントでAppコンポーネントが読み込まれる
+// ・app.jsは特殊なファイルでRouteコンポーネントをラップする
+// ・全ページで共通して実行させたいファイルを読み込む
+// ・全ページで共通して実行させたい処理を実行する
+// （ログインなど認証の監視とか）
+// ・全ページ共通のレイアウトを組み込む
+
+
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
