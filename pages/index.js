6bec9ea8907649dcdb328216a18a65cf3f0d34d3
naokihino178 <naoki.hino178@gmail.com>
2020-11-22 22:46:46 +0900

    next.jsチュートリアル（Implement getStaticProps）まで

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+// 自作したgetSortedPostsDataをインポート
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+// getStaticPropsはpageコンポーネントでのみ使用可能
+// getStaticPropを使っているということは、Next.jsに、「このページはデータの依存関係を持っている。
+// したがって、ビルド時にこのページをpre-renderしたいのであれば、最初にデータをとってきて、
+// 次にこの処理するという流れをやってくれよな」と伝える役割もある
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home ({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +29,20 @@ export default function Home() {
         <p>
           <a href="https://bz-vermillion.com/">私が最も好きなアーティストのサイトはこちら</a>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
