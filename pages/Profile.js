5669b73a29a1ae681daccafc3a307c50b468ceb1
naokihino178 <naoki.hino178@gmail.com>
2021-05-30 11:27:06 +0900

    #21 フォントサイを修正し、説明文を減らした

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,5 +1,5 @@
 import utilStyles from "../styles/utils.module.css";
-import styles from "../components/layout.module.css"; // stylesという名前でlayout.module.cssからインポート
+import styles from "../components/layout.module.css";
 import Link from "next/link";
 import Layout from "../components/layout";
 
@@ -17,6 +17,13 @@ const Profile = () => {
             className={`${utilStyles.headingSd} ${utilStyles.textAlignCenter}`}
           >
             hino
+          </p>
+          <p
+            className={`${utilStyles.headingSd} ${utilStyles.textAlignCenter}`}
+          >
+            フロントエンドエンジニアを
+            <br />
+            目指しています。
           </p>
           <a
             href="https://twitter.com/n__hino"
@@ -131,35 +138,57 @@ const Profile = () => {
           </section>
           <h2 className={utilStyles.outline}>製作物</h2>
           <section className={styles.main}>
+            <Link href="https://orenosetsuyakumeshi.herokuapp.com/">
+              <button className={styles.portfolioItem}>
+                <div className={styles.portfolioDesc}>
+                  <h2 className={utilStyles.headingMd}>レシピ管理アプリ</h2>
+                  <p className={utilStyles.headingSd}>
+                    (JavaScript(EJS), Express, MySQL, CircleCI, Heroku)
+                  </p>
+                  <ul>
+                    <li>
+                      レシピを作成、編集、削除することができるアプリです。
+                    </li>
+                    <li>
+                      調理工程を一工程ずつインプットフォームに入力できるようにしています。
+                    </li>
+                  </ul>
+                </div>
+                <img
+                  src="/images/recipeApp.jpeg"
+                  className={utilStyles.portfolioImg}
+                  alt="レシピ管理アプリ"
+                />
+              </button>
+            </Link>
             <Link href="https://quizapp-taupe.vercel.app/">
               <button className={styles.portfolioItem}>
                 <div className={styles.portfolioDesc}>
-                  <h2 className={utilStyles.headingLg}>クイズアプリ</h2>
-                  <p className={utilStyles.headingMd}>
+                  <h2 className={utilStyles.headingMd}>クイズアプリ</h2>
+                  <p className={utilStyles.headingSd}>
                     (React, TypeScript, Material-UI, Firebase)
                   </p>
                   <ul>
                     <li>
-                      日々の学習のため、シンプルで使い勝手の良いクイズアプリを作りました。
+                      問題の作成、編集、削除を行うことができるクイズアプリです。
                     </li>
-                    <li>Material UIを用いてモダンなUIを実現しています。</li>
-                    <li>
-                      某テレビ番組を参考に、回答を絞る機能などを実装してみました。
-                    </li>
+                    <li>Material-UIを用いてデザインを行っています。</li>
                   </ul>
                 </div>
                 <img
                   src="/images/quizApp.jpeg"
                   className={utilStyles.portfolioImg}
-                  // alt={name}
+                  alt="クイズアプリ"
                 />
               </button>
             </Link>
             <Link href="https://composition-app.vercel.app">
               <button className={styles.portfolioItem}>
                 <div className={styles.portfolioDesc}>
-                  <h2 className={utilStyles.headingLg}>作曲アプリ</h2>
-                  <p className={utilStyles.headingMd}>(React, TypeScript)</p>
+                  <h2 className={utilStyles.headingMd}>作曲アプリ</h2>
+                  <p className={utilStyles.headingSd}>
+                    (React, TypeScript, Firebase)
+                  </p>
                   <ul>
                     <li>
                       趣味の作曲を効率化できないかと考え、作成したアプリです。
@@ -167,39 +196,32 @@ const Profile = () => {
                     <li>
                       曲のキーを入力すると、ダイアトニックコードおよびよく使われるコード進行の一覧が表示されます。
                     </li>
-                    <li>
-                      趣味の作曲のスピードが上がりました。また、自分の趣味とプログラミングが繋がった実感があり、とても嬉しかったです。
-                    </li>
                   </ul>
                 </div>
                 <img
                   src="/images/compositionApp.jpeg"
                   className={utilStyles.portfolioImg}
-                  // alt={name}
+                  alt="作曲補助アプリ"
                 />
               </button>
             </Link>
-            <Link href="https://orenosetsuyakumeshi.herokuapp.com/">
+            <Link href="">
               <button className={styles.portfolioItem}>
                 <div className={styles.portfolioDesc}>
-                  <h2 className={utilStyles.headingLg}>節約レシピ管理アプリ</h2>
-                  <p className={utilStyles.headingMd}>
-                    (EJS, Express, MySQL, CircleCI, Heroku)
+                  <h2 className={utilStyles.headingMd}>ランキング投稿アプリ</h2>
+                  <p className={utilStyles.headingSd}>
+                    (React, TypeScript, Redux, Firebase(Authentication, Cloud
+                    Firestore, Storage))
                   </p>
                   <ul>
-                    <li>節約レシピを管理するアプリです。</li>
-                    <li>
-                      私が7年間の一人暮らしで開発した節約レシピをぜひご覧ください。
-                    </li>
-                    <li>
-                      調理工程を入力しやすくするため、一工程ずつインプットフォームを作成しました。
-                    </li>
+                    <li>ランキングを投稿するアプリです</li>
+                    <li>ユーザー情報の状態をReduxで管理しています。</li>
                   </ul>
                 </div>
                 <img
-                  src="/images/recipeApp.jpeg"
+                  src="/images/compositionApp.jpeg"
                   className={utilStyles.portfolioImg}
-                  // alt={name}
+                  alt="作曲補助アプリ"
                 />
               </button>
             </Link>
--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-    res.status(200).json({ text: 'Hello' })
-  }
-
-  // リクエストコード...200...成功
-  // 403...Forebidden Error
-  // 色々調べてみる
